f061a57ab48848248fb5e0ff6170e677409f11fc
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-11-08 21:03:21 +0100

    fix: Fix turn throttling to only apply to the sync, not the client visuals themselves

--- game/player_controller.gd
+++ game/player_controller.gd
@@ -7,6 +7,7 @@ var target_id: String
 var target_ref: WeakRef
 var move_requested_at: int
 var last_direction_update: int
+var last_synced_direction: GridDirections.Keys
 
 func _ready():
 	NetworkClient.ControlChanged.connect(_control_changed)
@@ -23,23 +24,25 @@ func _process(_delta):
 			direction.x -= 1
 		if Input.is_action_pressed("move_right"):
 			direction.x += 1
-	if direction != Vector2.ZERO:
-		var target = target_ref.get_ref() if target_ref else null
-		if !target && !target_id.is_empty():
-			target = _set_control_target(target_id)
-		if target:
-			var now = Time.get_ticks_msec()
+	var target = target_ref.get_ref() if target_ref else null
+	if !target && !target_id.is_empty():
+		target = _set_control_target(target_id)
+	if target:
+		var now = Time.get_ticks_msec()
+		if direction != Vector2.ZERO:
 			var grid_direction = GridDirections.from_input(direction)
 			if Input.is_action_pressed("use"):
-				if target.direction != grid_direction && now - last_direction_update > direction_update_throttle:
-					NetworkClient.SendTurnInput(grid_direction)
-					last_direction_update = now
+				target.direction = grid_direction
 			else:
 				var ready_to_move = !target.mobility || (!target.mobility.moving || target.mobility.move_progress > 0.9)
 				if (move_requested_at == 0 || now - move_requested_at > move_timeout) && ready_to_move:
 					var target_pos = GridDirections.apply_offset(target.map_position, grid_direction)
 					NetworkClient.SendMoveInput(target_pos)
 					move_requested_at = now
+		if target.direction != last_synced_direction && now - last_direction_update > direction_update_throttle:
+			NetworkClient.SendTurnInput(target.direction)
+			last_direction_update = now
+			last_synced_direction = target.direction
 
 func _control_changed(entity_id: String, _control_type: int, _seat: int):
 	_set_control_target(entity_id)
